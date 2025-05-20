f8b109b590aa4ed3112b55e4566191928a851871
Thomas Black-Roff <thblack-@c2r2p8.hive.fi>
2025-05-20 13:06:17 +0300

    Improved functionality and fixed invalid reads

--- get_next_line.c
+++ get_next_line.c
@@ -13,28 +13,79 @@
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*get_next_line(int fd)
+typedef struct	s_line {
+	char	*str;
+	int		newline;
+}	t_line;
+
+char	*str_append(char *s1, char *s2, int	len)
 {
-	char	*buf;
-	char	*line;
+	char	*new;
 	size_t	i;
 
+	if (!s1)
+	{
+		free(s1);
+		return (ft_strdup(s2));
+	}
+	i = 0;
+	while (s1[i])
+		i++;
+	new = malloc((i + len + 1) * sizeof(char));
+	ft_memcpy(new, s1, i);
+	free(s1);
+	ft_memcpy(new + i, s2, len);
+	new[i + len] = '\0';
+	return (new);
+}
+
+t_line	find_newline(int fd)
+{
+	t_line		temp;
+	static char	*leftover;
+	char		*buf;
+	size_t		i;
+
 	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buf)
-		return (NULL);
+	if (!leftover)
+	{
+		leftover = malloc(sizeof(char));
+		leftover[0] = '\0';
+	}
 	i = 0;
 	while (i < BUFFER_SIZE)
 	{
 		read(fd, &buf[i], 1);
-		if (buf[i] == '\n' || buf[i] == '\0')
+		if(buf[i] == '\n' || buf[i] == '\0')
 		{
-			line = ft_strdup(buf);
+			buf[i + 1] = '\0';
+			temp.str = ft_strjoin(leftover, buf);
+			temp.newline = 1;
 			free(buf);
-			return (line);
+			free(leftover);
+			leftover = NULL;
+			return (temp);
 		}
 		i++;
 	}
-	return (NULL);
+	buf[i] = '\0';
+	leftover = str_append(leftover, buf, i);
+	temp.newline = 0;
+	free(buf);
+	return (temp);
+}
+
+char	*get_next_line(int fd)
+{
+	t_line		temp;
+	char		*line;
+
+	temp.newline = 0;
+	while (temp.newline == 0)
+		temp = find_newline(fd);
+	line = ft_strdup(temp.str);
+	free(temp.str);
+	return (line);
 }
 
 int	main(int argc, char **argv)
